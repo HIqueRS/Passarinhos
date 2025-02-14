a2d7392dbb3355c862eb48cdcc2755bcff79f01b
Henrique Souza de Lima <henrique.lima@ldsoftware.local>
2025-02-14 10:20:59 -0300

    nem lembro mais

--- Assets/Scripts/DisplayText.cs
+++ Assets/Scripts/DisplayText.cs
@@ -1,6 +1,8 @@
 using System.Collections;
+using Microsoft.Unity.VisualStudio.Editor;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DisplayText : MonoBehaviour
 {
@@ -9,10 +11,15 @@ public class DisplayText : MonoBehaviour
 
     public float textSpeed;
 
+    public int characterPos;
+
+    public UnityEngine.UI.Image background;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         text.text = "";
+        characterPos = 0;
 
         StartCoroutine(PassText());
     }
@@ -20,14 +27,35 @@ public class DisplayText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            text.text = "";
+            StartCoroutine(PassText());
+        }
+        if(Input.GetKey(KeyCode.Space))
+        {
+            textSpeed = 0.01f;
+            background.color = new Color(232/255f,231/255f,166/255f,100/255f);
+        }
+        else{
+            textSpeed = 0.2f;
+            background.color = new Color(166/255f,222/255f,232/255f,100/255f);
+        }
     }
+
+
 
     private IEnumerator PassText()
     {  
-        for(int i = 0; i < nodeText.dialog.Length; i++)
+        for(; characterPos < nodeText.dialog.Length; characterPos++)
         {   
-            text.text += nodeText.dialog[i];
+            if(nodeText.dialog[characterPos] == '\n')
+            {
+                characterPos++;
+                break;
+            }
+            
+            text.text += nodeText.dialog[characterPos];
             yield return new WaitForSeconds(textSpeed);
 
         }
